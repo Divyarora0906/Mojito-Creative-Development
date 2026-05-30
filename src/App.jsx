602fedb22c0de82e865df2c1a47fe836a3ca0401
Divyarora0906 <divyarora0906@gmail.com>
2026-05-31 03:15:08 +0530

    feat(navbar): implement responsive navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/Navbar";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
-    <>
-      <h1 className="h-[100vh] flex-center text-3xl text-indigo-300">
-        Divy Arora
-      </h1>
-    </>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
